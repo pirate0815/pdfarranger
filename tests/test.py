b3a76aab0e36f1240d03e2c16fa4af4b07917805
pirate0815 <229522437+pirate0815@users.noreply.github.com>
2026-04-25 19:53:29 +0200

    Modify Test

--- tests/test.py
+++ tests/test.py
@@ -986,7 +986,7 @@ class TestBatch10(PdfArrangerTest):
 
     def test_02_disable_encryption(self):
         """Test export with encryption disabled"""
-        self._app().child(roleName="push button", name="Encryption").click()
+        self._app().child(roleName="push button", desciption="Encryption").click()
         self._mainmenu("Save As…")
         self._save_as_chooser("unencrypted.pdf", ["unencrypted.pdf"])
         with pikepdf.open(os.path.join(self.__class__.tmp, "unencrypted.pdf")):
@@ -995,7 +995,7 @@ class TestBatch10(PdfArrangerTest):
 
     def test_03_enable_encryption(self):
         """Test export with a password set manually"""
-        self._app().child(roleName="push button", name="Encryption").click()
+        self._app().child(roleName="push button", description="Encryption").click()
         dialog = self._app().child(roleName="dialog")
         passfield = dialog.child(roleName="password text")
         passfield.text = "foobar"
